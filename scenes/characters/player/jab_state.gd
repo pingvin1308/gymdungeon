1ebddf4c96be5427dd7504f64a4b94a9a29d63d1
Roman Trufanov <pingvin1308@gmail.com>
2025-02-04 02:25:05 +0400

    fix animation player for attack direction

--- scenes/characters/player/jab_state.gd
+++ scenes/characters/player/jab_state.gd
@@ -20,25 +20,19 @@ func _on_next_transitions() -> void:
 
 func _on_enter() -> void:
 	var mouse_position = get_viewport().get_mouse_position()
-	print("Mouse position: ", mouse_position)
 	var direction = (mouse_position - player.global_position).normalized()
 	hit_component_collision_shape.position = direction * attack_range + Vector2(0, player_height_offset)
 
-	# if player.player_direction == Vector2.UP:
-	# 	animated_sprite_2d.play("jab_up")
-	# 	hit_component_collision_shape.position = Vector2(0, -38)
-	# elif player.player_direction == Vector2.RIGHT:
-	# 	animated_sprite_2d.play("jab_right")
-	# 	hit_component_collision_shape.position = Vector2(18, -15)
-	# elif player.player_direction == Vector2.LEFT:
-	# 	animated_sprite_2d.play("jab_left")
-	# 	hit_component_collision_shape.position = Vector2(-18, -15)
-	# elif player.player_direction == Vector2.DOWN:
-	# 	animated_sprite_2d.play("jab_down")
-	# 	hit_component_collision_shape.position = Vector2(0, 12)
-	# else:
-	# 	animated_sprite_2d.play("jab_down")
-	# 	hit_component_collision_shape.position = Vector2(0, 10)
+	if direction.y > 0.5:
+		animated_sprite_2d.play("jab_down")
+	elif direction.y < -0.5:
+		animated_sprite_2d.play("jab_up")
+	elif direction.x > 0.5:
+		animated_sprite_2d.play("jab_right")
+	elif direction.x < -0.5:
+		animated_sprite_2d.play("jab_left")
+	else:
+		animated_sprite_2d.play("jab_down")
 		
 	hit_component_collision_shape.set_deferred("disabled", false)
 
