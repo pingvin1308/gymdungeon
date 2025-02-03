c9c23269d648c60a8a8d045821721cba08b84919
Roman Trufanov <pingvin1308@gmail.com>
2025-02-04 02:40:24 +0400

    fix basic enemy walking on top of players collider

--- scenes/Enemies/chase_state.gd
+++ scenes/Enemies/chase_state.gd
@@ -4,7 +4,7 @@ extends NodeState
 
 
 func _on_physics_process(_delta: float) -> void:
-	if enemy.is_chasing:
+	if enemy.is_chasing and enemy.target_player != null:
 		var player = enemy.target_player
 		var direction = (player.position - enemy.position).normalized()
 		enemy.velocity = direction * enemy.speed
