b9a4bdacd8f666a605eadab2720a49d586c50864
Roman Trufanov <pingvin1308@gmail.com>
2025-02-05 23:59:56 +0400

    add diagonal directions for walking

--- scripts/game_input_events.gd
+++ scripts/game_input_events.gd
@@ -4,7 +4,15 @@ static var direction: Vector2
 
 
 static func movement_input() -> Vector2:
-	if Input.is_action_pressed("walk_left"):
+	if Input.is_action_pressed("walk_up") and Input.is_action_pressed("walk_left"):
+		direction = Vector2.LEFT + Vector2.UP
+	elif Input.is_action_pressed("walk_up") and Input.is_action_pressed("walk_right"):
+		direction = Vector2.RIGHT + Vector2.UP
+	elif Input.is_action_pressed("walk_down") and Input.is_action_pressed("walk_left"):
+		direction = Vector2.LEFT + Vector2.DOWN
+	elif Input.is_action_pressed("walk_down") and Input.is_action_pressed("walk_right"):
+		direction = Vector2.RIGHT + Vector2.DOWN
+	elif Input.is_action_pressed("walk_left"):
 		direction = Vector2.LEFT
 	elif Input.is_action_pressed("walk_right"):
 		direction = Vector2.RIGHT
