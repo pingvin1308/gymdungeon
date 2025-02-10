b6db15285ee02f50f7f1c7d0c1e6b9c54e380629
Roman Trufanov <pingvin1308@gmail.com>
2025-02-11 03:36:34 +0400

    enhance notebook UI with stats and fitness menus, adding functionality for toggling visibility

--- scenes/ui/notebook.gd
+++ scenes/ui/notebook.gd
@@ -4,9 +4,34 @@ extends Control
 @export var strength_value: Label
 @export var dexterity_value: Label
 @export var endurance_value: Label
+@export var stats_menu: PanelContainer
+@export var fitness_menu: PanelContainer
+
+
+func _ready() -> void:
+	_toggle_node_process_node(stats_menu, false)
+	_toggle_node_process_node(fitness_menu, false)
 
 
 func _on_player_stats_changed(stats: Stats) -> void:
 	strength_value.text = str(stats.strength)
 	dexterity_value.text = str(stats.dexterity)
 	endurance_value.text = str(stats.endurance)
+
+
+func _on_stats_pressed() -> void:
+	_toggle_node_process_node(stats_menu, true)
+	_toggle_node_process_node(fitness_menu, false)
+
+
+func _on_fitness_pressed() -> void:
+	_toggle_node_process_node(stats_menu, false)
+	_toggle_node_process_node(fitness_menu, true)
+
+
+func _toggle_node_process_node(node: Node, toggle_visible: bool):
+	node.visible = toggle_visible
+	if not node.visible:
+		node.process_mode = PROCESS_MODE_DISABLED
+	else:
+		node.process_mode = PROCESS_MODE_INHERIT
